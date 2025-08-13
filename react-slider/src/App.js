5f6469f04bbde48c91f060da57dedafe4da7239c
AndrasCsorba <andreascs@mail.ch>
2025-08-13 18:28:08 +0200

    offset carousel

--- react-slider/src/App.js
+++ react-slider/src/App.js
@@ -4,24 +4,29 @@ import { useRef, useEffect, useState } from "react";
 import images from "./images";
 
 function App() {
-  const [width, setWidth] = useState(0);
+   const [width, setWidth] = useState(0);
+   const carousel = useRef();
 
-  return (
-    <div className="App">
-      <motion.div className="carousel">
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0 }}
-          className="inner-carousel">
-          {images.map((image, index) => (
-            <motion.div className="item" key={index}>
-              <img src={image} alt="" draggable="false" />
-            </motion.div>
-          ))}
-        </motion.div>
-      </motion.div>
-    </div>
-  );
+   useEffect(() => {
+     setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+   }, []);
+
+   return (
+     <div className="App">
+       <motion.div className="carousel">
+         <motion.div
+           drag="x"
+           dragConstraints={{ right: 0, left: -width }}
+           className="inner-carousel">
+           {images.map((image, index) => (
+             <motion.div className="item" key={index}>
+               <img src={image} alt="" draggable="false" />
+             </motion.div>
+           ))}
+         </motion.div>
+       </motion.div>
+     </div>
+   );
 }
 
 export default App;
